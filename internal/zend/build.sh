df1abe613ca2b6c928420bca462af1f733cf9806
midoks <midoks@163.com>
2021-09-19 03:44:45 +0800

    up

--- internal/zend/build.sh
+++ internal/zend/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 
-re2go zend_language_scanner.l -o zend_language_scanner.go -i -F -c
+re2go -i --no-generation-date --case-inverted -cbdF -o zend_language_scanner.go zend_language_scanner.l
+
+
+go run  zend_language_scanner.go
