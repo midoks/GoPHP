717db2511e8830ac2691cc5951d805323f76dda3
midoks <midoks@163.com>
2021-09-19 13:10:33 +0800

    Update build.sh

--- internal/zend/build.sh
+++ internal/zend/build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 
+re2go -i --no-generation-date --case-inverted -cbdF -o z.go z.l
+
+
 re2go -i --no-generation-date --case-inverted -cbdF -o zend_language_scanner.go zend_language_scanner.l
 
 
-go run  zend_language_scanner.go
+go run  zend_language_scanner.go zend_globals.go
