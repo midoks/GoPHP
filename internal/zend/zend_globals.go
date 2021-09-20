3dd805655b460a1cf2c48fb00ba276b92d1e7519
midoks <midoks@163.com>
2021-09-20 11:13:10 +0800

    Update zend_globals.go

--- internal/zend/zend_globals.go
+++ internal/zend/zend_globals.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"runtime"
+)
+
 type ZendPhpScannerGlobals struct {
 	YYLeng uint32
 
@@ -13,6 +18,22 @@ type ZendPhpScannerGlobals struct {
 
 var LanguageScannerGlobals ZendPhpScannerGlobals
 
+func init() {
+	LanguageScannerGlobals.YYCursor = 0
+	LanguageScannerGlobals.YYState = 1
+}
+
 func BEGIN(_state int32) {
 	LanguageScannerGlobals.YYState = _state
+}
+
+func FileLine() string {
+	_, fileName, fileLine, ok := runtime.Caller(1)
+	var s string
+	if ok {
+		s = fmt.Sprintf("%s:%d", fileName, fileLine)
+	} else {
+		s = ""
+	}
+	return s
 }
